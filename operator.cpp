1624d6c7bba7e13c8bd8873f9ce6be419c9305a0
Harinicode <119438229+Harinicode@users.noreply.github.com>
2023-09-16 12:03:57 +0530

    Create operator.cpp

---
+++ operator.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+class dis
+{
+    int f,i;
+    public:void getd()
+            {
+            cin>>f>>i;
+            }
+            friend dis operator+(dis d,dis d2);
+            void display()
+            {
+            cout<<f<<"\'"<<i<<"\""<<"\n";
+            }
+};
+dis operator+(dis d,dis d2)
+{
+    dis dd;
+    dd.f=0;
+    dd.i=d.i+d2.i;
+    if(dd.i>=12)
+    {
+    dd.i-=12;
+    dd.f++;
+    }
+    dd.f=d.f+d2.f;
+    return dd;
+}
+int main()
+{
+    dis d1,d2,d3;
+    d1.getd();
+    d2.getd();
+    d3=d1+d2;
+    d3.display();
+    return 0;
+}
